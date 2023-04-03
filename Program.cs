d6df9aa7644785d15127b4a73dc56484648ca610
GabrielLareglia <laregliagabriel@gmail.com>
2023-04-02 21:52:52 -0300

    Projeto concluiado

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿Console.Write("--- Imitando Dory ---\n");
+
+Console.WriteLine("Digite uma mensagem...: ");
+string mensagem = Console.ReadLine()!;
+
+string mensagemEmBaleies = mensagem
+    .Replace("a", "aaaaaaa")
+    .Replace("A", "AAAAAAA")
+    .Replace("e", "eeeeeee")
+    .Replace("E", "EEEEEEE")
+    .Replace("i", "iiiiiii")
+    .Replace("I", "IIIIIII")
+    .Replace("o", "ooooooo")
+    .Replace("O", "OOOOOOO")
+    .Replace("u", "uuuuuuu")
+    .Replace("U", "UUUUUUU")
+;
+
+Console.WriteLine($"\nEm baleiês:\n\n{mensagemEmBaleies}");
